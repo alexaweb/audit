def057c58793e5f831490fe29e6f1d0ae7976ddb
alexaweb <34030532+alexaweb@users.noreply.github.com>
2022-04-19 13:22:46 -0400

    relative

--- htdocs/home/home.php
+++ htdocs/home/home.php
@@ -8,7 +8,7 @@
     if(empty($_SESSION['user']))
     {
         // If they are not, we redirect them to the login page.
-        header("Location: home/login.php");
+        header("Location: login.php");
 
         // Remember that this die statement is absolutely critical.  Without it,
         // people can view your members-only content without logging in.
--- htdocs/home/login.php
+++ htdocs/home/login.php
@@ -94,8 +94,8 @@
             $_SESSION['user'] = $row;
 
             // Redirect the user to the private members-only page.
-            header("Location: arriendos/display_morosos.php");
-            die("Redirecting to: arriendos/display_morosos.php");
+            header("Location: ../arriendos/display_morosos.php");
+            die("Redirecting to: ../arriendos/display_morosos.php");
         }
         else
         {
@@ -117,7 +117,7 @@
 <link rel="icon"
   type="image/png"
   href="css/favicon.ico" />
-  <link rel="stylesheet" type="text/css" href="css/common_css.css" />
+  <link rel="stylesheet" type="text/css" href="../css/common_css.css" />
 </head>
 <section id="maincontent" >
 <h1>Login</h1>
@@ -130,5 +130,5 @@
     <br /><br />
     <button class="button-submit" type="submit" value="Login">login</button>
 </form>
-<a href="home/register.php">Registro de usuario</a>
+<a href="register.php">Registro de usuario</a>
 </section>
